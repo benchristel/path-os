2e9d196c76bf5f747bb3c45a64923a61f7e6586c
Ben Christel <bchristel@pivotal.io>
2021-07-11 13:56:13 -0700

    Move Clock component to its own file

---
+++ client/Clock.jsx
@@ -0,0 +1,57 @@
+// @flow
+
+import * as React from "react"
+import {useRedraw} from "./useRedraw.js"
+import {Timer} from "./timer.js"
+
+export function ClockController(): React.Node {
+  const redraw = useRedraw()
+  return <>
+    <ClockView now={new Date()}/>
+    <Timer onTick={redraw}/>
+  </>
+}
+
+function ClockView(props: {|
+  now: Date,
+|}): React.Node {
+  const {now} = props
+  return <>
+    {dayOfWeekAbbrev(now.getDay())}{", "}
+    {monthAbbrev(now.getMonth())}{" "}
+    {now.getDate()}{", "}
+    {now.toLocaleTimeString()}
+  </>
+}
+
+function monthAbbrev(m: number): string {
+  switch (m) {
+    case 0:  return "Jan"
+    case 1:  return "Feb"
+    case 2:  return "Mar"
+    case 3:  return "Apr"
+    case 4:  return "May"
+    case 5:  return "Jun"
+    case 6:  return "Jul"
+    case 7:  return "Aug"
+    case 8:  return "Sep"
+    case 9:  return "Oct"
+    case 10: return "Nov"
+    case 11: return "Dec"
+  }
+  return "???"
+}
+
+function dayOfWeekAbbrev(d: number): string {
+  switch (d) {
+    case 0: return "Sun"
+    case 1: return "Mon"
+    case 2: return "Tue"
+    case 3: return "Wed"
+    case 4: return "Thu"
+    case 5: return "Fri"
+    case 6: return "Sat"
+    case 7: return "Sun"
+  }
+  return "??"
+}
--- client/PathOs.jsx
+++ client/PathOs.jsx
@@ -6,8 +6,7 @@ import {
   MENU_BAR_HEIGHT_PX,
   BOTTOM_LETTERBOX_HEIGHT_PX,
 } from "./global-constants.js"
-import {useRedraw} from "./useRedraw.js"
-import {Timer} from "./timer.js"
+import {ClockController} from "./Clock.jsx"
 
 export function PathOs(): React.Node {
   return <>
@@ -28,53 +27,12 @@ export function PathOs(): React.Node {
       </div>
       <div className="menu-top-level menu-toolbar">
         <div className="menu-label">
-          <Clock/>
+          <ClockController/>
         </div>
       </div>
     </header>
     <footer className={css(footer)}/>
   </>
-}
-
-function Clock(): React.Node {
-  const redraw = useRedraw()
-  const now = new Date()
-  return <>
-    {dayOfWeekAbbrev(now.getDay())}, {monthAbbrev(now.getMonth())} {now.getDate()}, {now.toLocaleTimeString()}
-    <Timer onTick={redraw}/>
-  </>
-}
-
-function monthAbbrev(m: number): string {
-  switch (m) {
-    case 0:  return "Jan"
-    case 1:  return "Feb"
-    case 2:  return "Mar"
-    case 3:  return "Apr"
-    case 4:  return "May"
-    case 5:  return "Jun"
-    case 6:  return "Jul"
-    case 7:  return "Aug"
-    case 8:  return "Sep"
-    case 9:  return "Oct"
-    case 10: return "Nov"
-    case 11: return "Dec"
-  }
-  return "???"
-}
-
-function dayOfWeekAbbrev(d: number): string {
-  switch (d) {
-    case 0: return "Sun"
-    case 1: return "Mon"
-    case 2: return "Tue"
-    case 3: return "Wed"
-    case 4: return "Thu"
-    case 5: return "Fri"
-    case 6: return "Sat"
-    case 7: return "Sun"
-  }
-  return "??"
 }
 
 const desktop = css`
