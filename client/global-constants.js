707edc7d605841308ca5a2bb29e31ddf22182df0
Ben Christel <bchristel@pivotal.io>
2021-07-11 16:33:15 -0700

    Add Flow types to config constants

--- client/global-constants.js
+++ client/global-constants.js
@@ -1,6 +1,8 @@
-export const MENU_BAR_HEIGHT_PX = 21
+// @flow
+
+export const MENU_BAR_HEIGHT_PX: number = 21
 
 // The height of the black area at the bottom of the screen
 // that camouflages the URL preview that appears upon link
 // hover
-export const BOTTOM_LETTERBOX_HEIGHT_PX = 22
+export const BOTTOM_LETTERBOX_HEIGHT_PX: number = 22
